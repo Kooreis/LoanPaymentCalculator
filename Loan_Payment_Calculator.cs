c19d778a1c42a822c817a27d28bdfba5e476ee9f
Kooreis <kooreis.official@gmail.com>
2022-11-09 14:00:00 -0600

    Initial setup of the program. Declared all the necessary variables.

--- Loan_Payment_Calculator.cs
+++ Loan_Payment_Calculator.cs
@@ -1,4 +1,3 @@
-```CSharp
 using System;
 
 class Program
@@ -6,28 +5,5 @@ class Program
     static void Main(string[] args)
     {
         double loanAmount, annualInterestRate, monthlyInterestRate, loanLength, numberOfPayments, monthlyPayment, totalAmountPaid, totalInterestPaid;
-
-        Console.Write("Enter loan amount: ");
-        loanAmount = Convert.ToDouble(Console.ReadLine());
-
-        Console.Write("Enter annual interest rate (APR): ");
-        annualInterestRate = Convert.ToDouble(Console.ReadLine());
-
-        Console.Write("Enter loan length in years: ");
-        loanLength = Convert.ToDouble(Console.ReadLine());
-
-        monthlyInterestRate = annualInterestRate / 1200;
-        numberOfPayments = loanLength * 12;
-
-        monthlyPayment = (loanAmount * monthlyInterestRate) / (1 - Math.Pow(1 + monthlyInterestRate, -numberOfPayments));
-        totalAmountPaid = monthlyPayment * numberOfPayments;
-        totalInterestPaid = totalAmountPaid - loanAmount;
-
-        Console.WriteLine("\nMonthly Payment: " + Math.Round(monthlyPayment, 2));
-        Console.WriteLine("Total Amount Paid: " + Math.Round(totalAmountPaid, 2));
-        Console.WriteLine("Total Interest Paid: " + Math.Round(totalInterestPaid, 2));
-
-        Console.ReadKey();
     }
 }
-```
